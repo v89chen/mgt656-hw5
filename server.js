bdb085746f2c318bd121f5cc0be05f457d535c0a
Victoria <victoria.s.chen@yale.edu>
2016-10-07 02:30:57 +0000

    nickname

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 
 app.get('/', function(request, response){
     response.send('Hello MGT-656!');
+    
+app.get('/nickname', function(request, response){
+    response.send('glamorous-hawk');
 });
 
 app.listen(process.env.PORT || 4000);
